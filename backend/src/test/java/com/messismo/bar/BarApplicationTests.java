dc58c3d843040c1a066f42064d332324978762db
Martin Guido <guidomartin7@gmail.com>
2023-09-13 15:53:44 -0300

    spring initializer template backend

---
+++ backend/src/test/java/com/messismo/bar/BarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messismo.bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
