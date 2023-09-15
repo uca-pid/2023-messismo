4f5b05a8c30908f1f1392d327595712aa17e758b
Carla Baccari <carlabaccari8@gmail.com>
2023-09-15 15:46:32 -0300

    Add dynamic menu

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -2,108 +2,100 @@ import React from "react";
 import './Menu.css'
 
 function Menu() {
+    const products = [
+        {
+          nombre: 'Papas con cheddar',
+          categoria: 'Entradas',
+          descripcion: 'Este es el producto 1',
+          precio: '$3000',
+        },
+        {
+          nombre: 'Papas bravas',
+          categoria: 'Entradas',
+          descripcion: 'Este es el producto 2',
+          precio: '$3000',
+        },
+        {
+        nombre: 'Papas con cheddar',
+          categoria: 'Entradas',
+          descripcion: 'Este es el producto 1',
+          precio: '$2500',
+        },
+        {
+          nombre: 'Papas bravas',
+          categoria: 'Entradas',
+          descripcion: 'Este es el producto 2',
+          precio: '$50',
+        },
+        {
+        nombre: 'Negroni',
+          categoria: 'Tragos',
+          descripcion: 'Este es el producto 1',
+          precio: '$100',
+        },
+        {
+          nombre: 'Gin Tonic',
+          categoria: 'Tragos',
+          descripcion: 'Este es el producto 2',
+          precio: '$50',
+        },
+        {
+        nombre: 'Fernet',
+          categoria: 'Tragos',
+          descripcion: 'Este es el producto 1',
+          precio: '$100',
+        },
+        {
+          nombre: 'Hamburguesa Martin',
+          categoria: 'Platos',
+          descripcion: 'Este es el producto 2',
+          precio: '$50',
+        },
+        {
+        nombre: 'Pancho Carla',
+          categoria: 'Platos',
+          descripcion: 'Este es el producto 1',
+          precio: '$100',
+        },
+        {
+          nombre: 'Agua sin gas',
+          categoria: 'Bebidas sin alcohol',
+          descripcion: 'Agua sin gas 500ml ',
+          precio: '$50',
+        },
+        {
+            nombre: 'Agua con gas',
+            categoria: 'Bebidas sin alcohol',
+            descripcion: 'Agua sin gas 500ml ',
+            precio: '$50',
+          },
+
+      ];
+      const productsByCategory = {};
+
+      products.forEach((producto) => {
+          const categoria = producto.categoria;
+          if (!productsByCategory[categoria]) {
+            productsByCategory[categoria] = [];
+          }
+          productsByCategory[categoria].push(producto);
+      });
    
     return(
-       <div className="container">
-        <h1>Menu</h1>
-        <div className="entradas">
-            <p className="categories">Entradas</p>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-            <p className="text">Papas bravas</p>
-            <p className="text">$3000</p>
-            </div>
-            <div className="product">
-            <p className="text">Bueñuelos de espinaca</p>
-            <p className="text">$3000</p>
-            </div>
-            <div className="product">
-            <p className="text">Bastones de muzzarella</p>
-            <p className="text">$3000</p>
-            </div>
+        <div className="container">
+            <h1>Menu</h1>
+            {Object.keys(productsByCategory).map((categoria) => (
+                <div key={categoria} className="entradas">
+                    <p className="categories">{categoria}</p>
+                    {productsByCategory[categoria].map((producto, index) => (
+                        <div key={index} className="product">
+                            <p className="text">{producto.nombre}</p>
+                            <p className="text">{producto.precio}</p>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
-        <div className="entradas">
-            <p className="categories">Platos</p>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-        </div>
-        <div className="entradas">
-            <p className="categories">Tragos</p>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-        </div>
-        <div className="entradas">
-            <p className="categories">Bebidas</p>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-
-        </div>
-        <div className="entradas">
-            <p className="categories">Postres</p>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-            <div className="product">
-                <p className="text">Papas con cheddar</p>
-                <p className="text">$3000</p>
-            </div>
-
-        </div>
-       </div>
 
     )
 }
