d61e9eeb5c92d9c50d6bc6ecc73926244f4fa842
Karen <karen-herrmann@hotmail.com>
2023-09-30 17:05:15 -0300

    improved resources view

--- frontend/src/screens/Resources.js
+++ frontend/src/screens/Resources.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import "../Resources.css";
 import { useSelector, useDispatch } from "react-redux";
 // import { acceptUser, rejectUser, deleteUser } from '../redux/userSlice';
 import { validateUser, upgradeUser } from "../redux/userSlice";
@@ -7,10 +8,19 @@ import Navbar from "../components/Navbar";
 import userService from "../services/user.service";
 import { Navigate } from "react-router-dom";
 import employeeService from "../services/employees.service";
+import user1 from '../images/users/user-1.png';
+import user2 from '../images/users/user-2.png';
+import user3 from '../images/users/user-3.png';
+import user4 from '../images/users/user-4.png';
+import user5 from '../images/users/user-5.png';
+import user6 from '../images/users/user-6.png';
+import user7 from '../images/users/user-7.png';
+
+const userimages = [user1,user2,user3,user4,user5,user6,user7];
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
+  /* display: flex;
+  flex-direction: column; */
 `;
 
 const MainContent = styled.div`
@@ -30,97 +40,88 @@ const UserContainer = styled.div`
   display: flex;
   margin-top: 20px;
   flex-wrap: wrap;
-
-  @media (max-width: 800px) {
-    display: block;
-  }
 `;
 
 const Resource = styled.div`
   display: flex;
   flex-wrap: wrap;
   font-family: "Roboto";
+  gap: 2rem;
+  margin: 1rem;
+  /* margin: 2rem;
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: 1fr;
+  justify-content: center; */
+  text-transform: uppercase;
 
-  .user-data {
-    border-radius: 3px;
-    padding: 1rem 3.5rem;
-    border: 1px solid black;
-    background-color: rgb(164, 212, 204, 0.7);
-    color: black;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    text-align: center;
-    font-size: 14px;
-    margin: 10px;
+  @media screen and (min-width:600px){
+    grid-template-columns: repeat(auto-fit, minmax(24rem, 26rem));
   }
 
-  h3 {
-    font-size: 20px;
-    padding: 1rem;
+  .card{
+    background-color: rgb(164, 212, 204, 0.6);
+    border-radius: 0.2rem;
+    color: black;
+    min-width: 28rem;
+    display: flex;
+    font-size: 1.4rem;
+    text-align: center;
+    overflow: hidden;
+    align-items: center;
+
+    @media screen and (min-width:600px){
+      flex-direction: column;
+      text-align: center;
+    }
+
+    @media(max-width: 768px){
+      width: 100%;
+      height: 8em;
+    }
+  }
+
+  .card-body{
+    flex-grow: 1;
+    overflow: auto;
+    margin: 1em;
+  }
+
+  .card-username{
+  }
+
+  .card-email{
+
   }
 `;
 
 const Button = styled.button`
-  border-radius: 3px;
-  padding: 1rem;
-  margin-top: 1rem;
-  border: 1px solid black;
+  border: none;
+  border-top: 1px solid rgb(164, 212, 204, 0.2);
+  background-color: transparent;
+  font-size: 2rem;
+  font-weight: bold;
   color: black;
-  text-transform: uppercase;
+  width: 100%;
   cursor: pointer;
-  letter-spacing: 1px;
-  box-shadow: 0 3px #999;
-  font-family: "Roboto";
-  text-align: center;
-  font-size: 14px;
+  text-transform: uppercase;
+  padding-top: 1rem;
+  margin-top: 0.5em;
 
-  &.button-accept {
-    background-color: #a4d4cc;
-    margin-right: 1rem;
-
-    &:hover {
-      background-color: green;
-    }
+  &:hover{
+    color: green;
   }
+`;
 
-  &.button-reject {
-    background-color: #a4d4cc;
+const UserImage = styled.img`
+  width: 10rem;
+  height: 100%;
+  object-fit: cover;
 
-    &:hover {
-      background-color: red;
-    }
-  }
-
-  &.button-delete {
-    background-color: #a4d4cc;
-
-    &:hover {
-      background-color: red;
-    }
-  }
-
-  &.button-validate {
-    background-color: #a4d4cc;
-    margin-right: 1rem;
-
-    &:hover {
-      background-color: green;
-    }
-  }
-
-  &.button-upgrade {
-    background-color: #a4d4cc;
-    margin-right: 1rem;
-
-    &:hover {
-      background-color: green;
-    }
-  }
-
-  &:active {
-    background-color: #a4d4cc;
-    box-shadow: 0 3px #666;
-    transform: translateY(4px);
+  @media screen and (min-width:600px){
+    width: 100%;
+    height: 22rem;
+    object-fit: cover;
   }
 `;
 
@@ -135,6 +136,8 @@ const Subheader = styled.h2`
 `;
 
 function Resources() {
+  const [randomImage, setRandomImage] = useState(null);
+
   const { user: currentUser } = useSelector((state) => state.auth);
   const clicked = useSelector((state) => state.navigation.clicked);
 
@@ -153,6 +156,11 @@ function Resources() {
       .catch((error) => {
         console.error("Error al obtener la lista de empleados:", error);
       });
+  }, []);
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * userimages.length);
+    setRandomImage(userimages[randomIndex]);
   }, []);
 
   const handleValidate = (id) => {
@@ -188,48 +196,66 @@ function Resources() {
     return <Navigate to="/homepage" />;
   }
 
-  const renderUser = (user) => (
-    <div key={user.id} className="user-data">
-      <h3 className="user-username">{user.username}</h3>
-      <p className="user-email">{user.email}</p>
-      <p className="user-role">{user.role}</p>
-      {user.role === "EMPLOYEE" && (
-        <div>
-          <Button
-            className="button-validate"
-            onClick={() => handleValidate(user.id)}
-          >
-            Validate
-          </Button>
-          {/* <Button className="button-reject" onClick={() => handleReject(user.id)}>Reject</Button> */}
-        </div>
-      )}
-      {user.role === "VALIDATEDEMPLOYEE" && (
-        <div>
-          { actualUserRole === "ADMIN" && (
-          <Button
-            className="button-upgrade"
-            onClick={() => handleUpgrade(user.id)}
-          >
-            Upgrade
-          </Button>
+  const renderUser = (user) => {
+
+    const randomIndex = Math.floor(Math.random() * userimages.length);
+    const randomImage = userimages[randomIndex];
+
+    return(
+      <div key={user.id} className="card">
+
+        <UserImage src={randomImage} alt={`User ${user.id}`} />
+
+        <div className="card-body">
+          <h2 className="card-username">{user.username}</h2>
+          <p className="card-email">{user.email}</p>
+
+          {user.role === "EMPLOYEE" && (
+            <div>
+              <Button
+                className="button-validate"
+                onClick={() => handleValidate(user.id)}
+              >
+                Validate
+              </Button>
+            </div>
           )}
+
+          {user.role === "VALIDATEDEMPLOYEE" && (
+            <div>
+              { actualUserRole === "ADMIN" && (
+              <Button
+                className="button-upgrade"
+                onClick={() => handleUpgrade(user.id)}
+              >
+                Upgrade
+              </Button>
+              )}
+            </div>
+          )}
+
         </div>
-        
-      )}
-    </div>
-  );
 
-  const renderAdmin = (user) => (
-    <div key={user.id} className="user-data">
-      <h3 className="user-username">{user.username}</h3>
-      
-      {actualUserRole === "ADMIN" && <p className="user-email">{user.email}</p>}
+      </div>
+  )};
 
-      <p className="user-role">{user.role}</p>
+  const renderAdmin = (user) => {
 
-    </div>
-  );
+    const randomIndex = Math.floor(Math.random() * userimages.length);
+    const randomImage = userimages[randomIndex];
+
+    return(
+      <div key={user.id} className="card">
+
+        <UserImage src={randomImage} alt={`User ${user.id}`} />
+
+        <div className="card-body">
+          <h2 className="card-username">{user.username}</h2>
+          {actualUserRole === "ADMIN" && <p className="card-email">{user.email}</p>}
+        </div>
+
+      </div>
+  )};
 
   return (
     <Container>
@@ -241,7 +267,7 @@ function Resources() {
             0 && (
             <UserItem>
               <Subheader>Employees</Subheader>
-              <Resource>
+              <Resource> 
                 {allEmployees
                   .filter((user) => user.role === "EMPLOYEE")
                   .map(renderUser)}
