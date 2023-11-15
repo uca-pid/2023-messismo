e4b93101946aefe20b34f1191d62386636070df0
Carla Baccari <carlabaccari8@gmail.com>
2023-11-15 16:11:22 -0300

    Change dashboards api url

--- frontend/src/services/dashboard.service.js
+++ frontend/src/services/dashboard.service.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import authHeader from "./auth-header";
-//import apiUrl from "../deploy";
+import apiUrl from "../deploy";
 
-const apiUrl = "localhost:8080";
+
 
 const API_URL = apiUrl + "/api/v1/manager/dashboard/getDashboard";
 
